49937ee771bb3b1642f1d099516beeb9d2002091
Yurchenko <iwwwi2015@gmail.com>
2023-08-14 09:02:52 -0400

    Some clear data

--- resources/views/dashboard/title.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="dashboard-title">
-    Resources
-</div>
-<div class="dashboard-links">
-    <a href="https://github.com/open-developer-org/open-developer" target="_blank">Github</a> -
-    <a href="http://open-developer.org/docs"  target="_blank">Documentation</a> -
-    <a href="http://open-developer.org/demo"  target="_blank">Demo</a>
-</div>
--- src/Controllers/Dashboard.php
+++ src/Controllers/Dashboard.php
@@ -10,14 +10,6 @@ class Dashboard
     /**
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    public static function title()
-    {
-        return view('developer::dashboard.title');
-    }
-
-    /**
-     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
-     */
     public static function environment()
     {
         $envs = [
@@ -38,72 +30,6 @@ class Dashboard
         ];
 
         return view('developer::dashboard.environment', compact('envs'));
-    }
-
-    /**
-     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
-     */
-    public static function extensions()
-    {
-        $extensions = [
-            'helpers' => [
-                'name' => 'open-admin-ext/helpers',
-                'link' => 'https://github.com/open-admin-org/helpers',
-                'icon' => 'cogs',
-            ],
-            'log-viewer' => [
-                'name' => 'open-admin-ext/log-viewer',
-                'link' => 'https://github.com/open-admin-org/log-viewer',
-                'icon' => 'database',
-            ],
-            'backup' => [
-                'name' => 'open-admin-ext/backup',
-                'link' => 'https://github.com/open-admin-org/backup',
-                'icon' => 'copy',
-            ],
-            'config' => [
-                'name' => 'open-admin-ext/config',
-                'link' => 'https://github.com/open-admin-org/config',
-                'icon' => 'toggle-on',
-            ],
-            'api-tester' => [
-                'name' => 'open-admin-ext/api-tester',
-                'link' => 'https://github.com/open-admin-org/api-tester',
-                'icon' => 'sliders-h',
-            ],
-            'media-manager' => [
-                'name' => 'open-admin-ext/media-manager',
-                'link' => 'https://github.com/open-admin-org/media-manager',
-                'icon' => 'file',
-            ],
-            'scheduling' => [
-                'name' => 'open-admin-ext/scheduling',
-                'link' => 'https://github.com/open-admin-org/scheduling',
-                'icon' => 'clock',
-            ],
-            'reporter' => [
-                'name' => 'open-admin-ext/reporter',
-                'link' => 'https://github.com/open-admin-org/reporter',
-                'icon' => 'bug',
-            ],
-            'redis-manager' => [
-                'name' => 'open-admin-ext/redis-manager',
-                'link' => 'https://github.com/open-admin-org/redis-manager',
-                'icon' => 'flask',
-            ],
-            'grid-sortable' => [
-                'name' => 'open-admin-ext/grid-sortable',
-                'link' => 'https://github.com/open-admin-org/grid-sortable',
-                'icon' => 'arrows-alt-v',
-            ],
-        ];
-
-        foreach ($extensions as &$extension) {
-            $name = explode('/', $extension['name']);
-            $extension['installed'] = array_key_exists(end($name), Developer::$extensions);
-        }
-
-        return view('developer::dashboard.extensions', compact('extensions'));
     }
 
     /**
